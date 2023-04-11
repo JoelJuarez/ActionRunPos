fa097b16ad71e6f98d249bc50f401e72c9eeb990
Joel Juarez <40305419+JoelJuarez@users.noreply.github.com>
2023-04-10 18:41:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ try {
          let finalSing = versionName.indexOf(")");
          let qaVersion = versionName.substring(initSing + 1, finalSing)
             
-         core.setOutput( "qa-version-number",`${finalNewVersion}`);
+         core.setOutput( "qa-version-number",`${qaVersion}`);
             
           console.log(`lastPartVersion -> ${versionName.substring(initSing + 1, finalSing)}`);
             
